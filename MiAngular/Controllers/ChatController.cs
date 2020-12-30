fa57efb042d0c798953ed17928f4cf70a613a7c8
lucho <lucianofiamingo@gmail.com>
2020-12-30 11:33:08 -0300

    Query with LINQ in ChatController.

---
+++ MiAngular/Controllers/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using MiAngular.Models;
+using MiAngular.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatController : Controller
+    {
+        private readonly MyDBContext dBContext;
+
+        public ChatController(MyDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<MessageVM> Message()
+        {
+            List<MessageVM> listMsgs = (from msg in dBContext.Message
+                                        select new MessageVM
+                                        {
+                                            Id = msg.Id,
+                                            Name = msg.Name,
+                                            Text = msg.Text
+                                        }).ToList();
+
+            return listMsgs;
+        }
+    }
+}
